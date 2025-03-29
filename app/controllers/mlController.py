b1c8acedeae6e59a31c925e1faaa11596d534fc6
nathalia-dssb <nath.dsbb@gmail.com>
2025-03-29 05:29:45 -0600

    shsd

--- app/controllers/mlController.py
+++ app/controllers/mlController.py
@@ -1,19 +1,28 @@
 from PIL import Image
 import requests
+import dicti
+import re
 
 from transformers import CLIPProcessor, CLIPModel
 
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+def analyze(img_url, classes):
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-print(image)
+    url = img_url
+    image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+    inputs = processor(text=classes, images=image, return_tensors="pt", padding=True)
 
-outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
-print(probs)
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+    probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+
+    numbers = re.findall(r"[-+]?\d*\.\d+e[-+]?\d+", str(probs))
+    cleaned_numbers = [float(num) for num in numbers]
+
+    results= dict(zip(classes, cleaned_numbers))
+    return results
+
+analyze("https://storage.googleapis.com/violence_images_bucket/violence_dataset/noviolence/NV_992.mp4_frame115.jpg", dicti.siniestro)
