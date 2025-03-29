4a4de715bef2fd97f0951ae8e82fb8dc972ec1db
Luis <lgarcia164@alumnos.uaq.mx>
2025-03-29 03:43:03 -0600

    add Controladores para ML

---
+++ app/controllers/mlGoogleController.py
@@ -0,0 +1,68 @@
+from typing import Dict, List, Union
+
+from google.cloud import aiplatform
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Value
+from io import BytesIO
+from PIL import Image 
+import requests
+import base64
+
+def predict_custom_trained_model_sample(
+    project: str,
+    endpoint_id: str,
+    instances: Union[Dict, List[Dict]],
+    location: str = "us-central1",
+    api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+):
+    """
+    `instances` can be either single instance of type dict or a list
+    of instances.
+    """
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    # The format of each instance should conform to the deployed model's prediction input schema.
+    instances = instances if isinstance(instances, list) else [instances]
+    instances = [
+        json_format.ParseDict(instance_dict, Value()) for instance_dict in instances
+    ]
+    parameters_dict = {}
+    parameters = json_format.ParseDict(parameters_dict, Value())
+    endpoint = client.endpoint_path(
+        project=project, location=location, endpoint=endpoint_id
+    )
+    response = client.predict(
+        endpoint=endpoint, instances=instances, parameters=parameters
+    )
+    print(response)
+    print("response")
+    print(" deployed_model_id:", response.deployed_model_id)
+    # The predictions are a google.protobuf.Value representation of the model's predictions.
+    predictions = response.predictions
+    print(predictions)
+    for prediction in predictions:
+        print(" prediction:", prediction)
+
+def download_image(url):
+    response = requests.get(url)
+    return Image.open(BytesIO(response.content))
+
+def image_to_base64(image, format="JPEG"):
+    buffer = BytesIO()
+    image.save(buffer, format=format)
+    image_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return image_str
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image1 = download_image(url)
+image = image_to_base64(image1)
+
+predict_custom_trained_model_sample(
+    project="672257461501",
+    endpoint_id="648418290783223808",
+    location="us-central1",
+    instances={ "text": ["a photo of a cat", "a photo of a dog"], "image": image}
+)
